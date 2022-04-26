857cda5cad72c708e99145ff08921016c12b9cb6
Ambala Prashanth <89448613+86prashanth@users.noreply.github.com>
2022-04-26 16:39:22 +0530

    'firstcommit'

---
+++ modelapplication/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Art(models.Model):
+    pub_date=models.DateField()
+    headline=models.CharField(max_length=200)
+    content =models.TextField()
+    def __str__(self):
+        return self.headline
+
+    
